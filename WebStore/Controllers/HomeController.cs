275db991f71b2386c4a85a350c5f53f9fe91b10c
Vitaly Kalinin <vital5991@gmail.com>
2021-05-21 15:29:23 +0300

    Создание первого контроллера

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private  readonly IConfiguration _Configuration;
+
+        public HomeController(IConfiguration Configuration)
+        {
+            _Configuration = Configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return Content("Hello from controller!");
+        }
+
+        public IActionResult SecondAction()
+        {
+            return Content(_Configuration["Greetings"]);
+        }
+    }
+}
