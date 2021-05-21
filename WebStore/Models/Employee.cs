e216410351685093d2df91a9d8e327c5e84cc66e
Vitaly Kalinin <vital5991@gmail.com>
2021-05-21 15:39:26 +0300

    Сделана модель сотрудника

---
+++ WebStore/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronomyc { get; set; }
+        public int Age { get; set; }
+
+    }
+}
