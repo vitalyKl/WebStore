6affe1c302e63ae3fe605d458f6841b2d756389c
Vitaly Kalinin <vital5991@gmail.com>
2021-05-21 15:20:15 +0300

    Добавление инфраструктуры сервисов MVC

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -21,6 +21,7 @@ namespace WebStore
         }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -34,10 +35,11 @@ namespace WebStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.MapGet("/greetings", async context =>
                 {
                     await context.Response.WriteAsync(Configuration["Greetings"]);
                 });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
