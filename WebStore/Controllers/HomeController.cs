85b28bdceda95dc5fbf1e6b23cf53b2f10c3de07
Vitaly Kalinin <vital5991@gmail.com>
2021-05-21 15:53:18 +0300

    Настроены ссылки между страницами

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,11 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _Employees = new()
+        {
+            new Employee { Id = 1, Age = 20, Name = "Александр", Patronomyc = "Сергеевич", Surname = "Пушкин" },
+            new Employee { Id = 2, Age = 38, Name = "Вячеслав", Patronomyc = "Михайлович", Surname = "Губерниев" },
+            new Employee { Id = 3, Age = 31, Name = "Дмитрий", Patronomyc = "Витальевич", Surname = "Соловей" }
+        };
+
         private  readonly IConfiguration _Configuration;
 
         public HomeController(IConfiguration Configuration)
@@ -24,6 +32,11 @@ namespace WebStore.Controllers
         public IActionResult SecondAction()
         {
             return Content(_Configuration["Greetings"]);
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_Employees);
         }
     }
 }
