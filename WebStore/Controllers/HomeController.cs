a5f3486b37de46f339864910cd499852dea7e676
Vitaly Kalinin <vital5991@gmail.com>
2021-05-25 11:25:53 +0300

    Добавлена "карточка сотрудника"

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -12,9 +12,9 @@ namespace WebStore.Controllers
     {
         private static readonly List<Employee> _Employees = new()
         {
-            new Employee { Id = 1, Age = 20, Name = "Александр", Patronomyc = "Сергеевич", Surname = "Пушкин" },
-            new Employee { Id = 2, Age = 38, Name = "Вячеслав", Patronomyc = "Михайлович", Surname = "Губерниев" },
-            new Employee { Id = 3, Age = 31, Name = "Дмитрий", Patronomyc = "Витальевич", Surname = "Соловей" }
+            new Employee { Id = 1, Age = 20, Name = "Александр", Patronomyc = "Сергеевич", Surname = "Пушкин", Department = "Продажи", HiringDate = new DateTime(2010, 11, 15), Degree = "Высшее" },
+            new Employee { Id = 2, Age = 38, Name = "Вячеслав", Patronomyc = "Михайлович", Surname = "Губерниев", Department = "Логистика", HiringDate = new DateTime(2010, 11, 05), Degree = "Среднее" },
+            new Employee { Id = 3, Age = 31, Name = "Дмитрий", Patronomyc = "Витальевич", Surname = "Соловей", Department = "Закупки", HiringDate = new DateTime(2010, 11, 01), Degree = "Высшее" }
         };
 
         private  readonly IConfiguration _Configuration;
@@ -37,6 +37,11 @@ namespace WebStore.Controllers
         public IActionResult Employees()
         {
             return View(_Employees);
+        }
+
+        public IActionResult Details(int Id)
+        {
+            return View(_Employees[Id-1]);
         }
     }
 }
--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -12,6 +12,9 @@ namespace WebStore.Models
         public string Surname { get; set; }
         public string Patronomyc { get; set; }
         public int Age { get; set; }
+        public string Department { get; set; }
+        public DateTime HiringDate { get; set; }
+        public string Degree { get; set; }
 
     }
 }
