04cccd916f7139f5a5d73677e14c9c403cbedd2b
Vitaly Kalinin <vital5991@gmail.com>
2021-06-12 22:52:27 +0300

    Базовые классы и интерфейсы сущностей в доменных моделях

---
+++ WebStore.Domain/Entities/Base/Interfaces/INamedEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace WebStore.Domain.Entities.Base.Interfaces
+{
+    public interface INamedEntity: IEntity
+    {
+        string Name { get; }
+    }
+
+}
---
+++ WebStore.Domain/Entities/Base/Interfaces/IOrderedEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace WebStore.Domain.Entities.Base.Interfaces
+{
+    public interface IOrderedEntity: IEntity
+    {
+        int Order { get; }
+    }
+
+}
