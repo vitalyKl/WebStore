dadd9b9e1c8ddc1b903f41e8f7ac17af5c0a2921
Vitaly Kalinin <vital5991@gmail.com>
2021-06-16 17:08:47 +0300

    Добавлена возможность переходить по ссылкам в секциях и брендам

--- WebStore/Controllers/CatalogController.cs
+++ WebStore/Controllers/CatalogController.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain;
 using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -17,7 +19,24 @@ namespace WebStore.Controllers
         }
         public IActionResult Index(int? BrandId, int? SectionId)
         {
-            return View();
+            var filter = new ProductFilter
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+            };
+            var products = _ProductData.GetProducts(filter);
+            return View(new CatalogViewModel
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+                Products = products.OrderBy(p => p.Order).Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl,
+                })
+            });
         }
     }
 }
---
+++ WebStore/ViewModels/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+    public class CatalogViewModel
+    {
+        public int? SectionId { get; set; }
+        public int? BrandId { get; set; }
+        public IEnumerable<ProductViewModel> Products { get; set; }
+    }
+}
